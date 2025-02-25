2974cebb93cff99d65566aec12402648bcd73144
Kevin El Khoury <dark.kevin1122@gmail.com>
2025-02-25 14:22:09 +0100

    saving added

--- src/main/java/com/example/project/Controller.java
+++ src/main/java/com/example/project/Controller.java
@@ -2,113 +2,193 @@ package com.example.project;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventTarget;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.web.HTMLEditor;
 
-import java.awt.*;
+import java.awt.Desktop;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JFileChooser;
+import javax.swing.text.html.HTML;
 
 public class Controller {
     @FXML
-            private TabPane tabPane;
+    private TabPane tabPane;
     @FXML
-            private SplitPane splitPane;
+    private SplitPane splitPane;
     @FXML
-            private Button SBCloseBtn;
+    private Button SBCloseBtn;
     @FXML
-            private AnchorPane rightPane;
+    private AnchorPane rightPane;
 
-    public void initialize() {
-        rightPane.layoutXProperty().addListener((obs, oldX, newX) -> {
-            SBCloseBtn.setLayoutX(newX.doubleValue() - 30.0);
-        });
-    }
-
-    Stage stage = new Stage();
     int clickCount = 0;
 
-    public void newFile() {
-        BorderPane root = new BorderPane();
-        TextField txtField = new TextField();
-        Button submitBtn = new Button("submit");
+    public void initialize() {
+        rightPane.layoutXProperty().addListener((_, _, newX) ->
+                SBCloseBtn.setLayoutX(newX.doubleValue() - 30.0)
+        );
+    }
 
-        submitBtn.setOnMouseClicked(event -> {
-            String fileName = txtField.getText();
+    public Menu getFileMenu(EventHandler eventHandler1, EventHandler eventHandler2) {
+        Menu file = new Menu("file");
+        MenuItem save = new MenuItem("save");
+        MenuItem saveAs = new MenuItem("save as");
+
+        saveAs.setOnAction(eventHandler1);
+        save.setOnAction(eventHandler2);
+
+        file.getItems().addAll(
+                save,
+                saveAs
+        );
+        return file;
+    }
+
+    public void openSaveFileChooser(Stage stage, File file) {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("save file");
+        fileChooser.showSaveDialog(stage);
+
+        if (file != null) {
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write("Hello, this is the saved content!");
+                System.out.println("File saved: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void newFile() {
+        Stage stage = new Stage();
+
+        // open the popup
+        // the root node
+        BorderPane root = new BorderPane();
+
+        // the input of the user (the name of the file)
+        TextField textField = new TextField();
+        textField.setPromptText("file name");
+
+        // the submit button
+        Button submit = new Button("submit");
+
+        // adding an event handler for the submit button so that it would do its job
+        submit.setOnAction(_ -> {
+            // saving the file
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("text files", "*.txt"));
+            File file = fileChooser.showSaveDialog(stage);
+
+            if (file != null) {
+                try {
+                    if (file.createNewFile()) {
+                        System.out.println("file created successfully");
+                    } else {
+                        System.out.println("file already exists");
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else {
+                System.out.println("file is null");
+            }
+
+            // adding the tab
+            String fileName = textField.getText();
             if (!fileName.isEmpty()) {
-                Tab tab = new Tab(fileName);
+                Group tabRoot = new Group();
                 HTMLEditor editor = new HTMLEditor();
+                Tab tab = new Tab(fileName);
+
+                editor.setLayoutY(30.0);
+
+                tabRoot.getChildren().add(editor);
+                tabRoot.getChildren().add(
+                        new MenuBar(
+                        getFileMenu(
+                                _ -> System.out.println("save as"),
+                                _ -> openSaveFileChooser(stage, file)
+                        ))
+                );
 
                 editor.setPrefSize(600.0, 700.0);
-                tab.setContent(editor);
+                tab.setContent(tabRoot);
 
                 Platform.runLater(() -> tabPane.getTabs().add(tab));
             }
             stage.close();
         });
 
-        root.setCenter(txtField);
-        root.setBottom(submitBtn);
+        // adding the nodes to the root node
+        root.setCenter(textField);
+        root.setBottom(submit);
 
+        // setting the scene
         stage.setScene(new Scene(root));
         stage.show();
     }
+
     public void openFile(ActionEvent event) {
         String buttonText = ((Button) event.getSource()).getText();
 
-        try {
-            JFileChooser fileChooser = new JFileChooser();
+        JFileChooser fileChooser = new JFileChooser();
+        if (buttonText == "open file") {
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        } else {
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        }
 
+        int result = fileChooser.showOpenDialog(null);
+        File selectedFile = new File(fileChooser.getSelectedFile().getAbsolutePath());
+
+        if (result == JFileChooser.APPROVE_OPTION) {
             if (buttonText.equals("open file")) {
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            } else if (buttonText.equals("open folder")) {
-                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            }
-            fileChooser.setCurrentDirectory(new File("C:/"));
+                Tab tab = new Tab(selectedFile.getName());
+                tab.setClosable(true);
 
-            fileChooser.setBackground(Color.white);
+                HTMLEditor editor = new HTMLEditor();
+                editor.setPrefSize(
+                        600.0,
+                        700.0
+                );
 
-            int result = fileChooser.showOpenDialog(null);
+                Group root = new Group();
+                root.getChildren().addAll(
+                        new MenuBar(
+                                getFileMenu(
+                                        _ -> System.out.println("save"),
+                                        _ -> openSaveFileChooser(null, selectedFile)
+                                )
+                        ),
+                        editor
+                );
+                tab.setContent(root);
 
-            System.out.println("result: " + result);
-
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                System.out.println("file path: " + selectedFile);
-
-                if (!Desktop.isDesktopSupported()) {
-                    System.out.println("file not supported");
-                } else {
-                    if (buttonText.equals("open file")) {
-                        Tab tab = new Tab(selectedFile.getName());
-                        HTMLEditor editor = new HTMLEditor();
-
-                        editor.setPrefSize(600.0, 700.0);
-
-                        tab.setContent(editor);
-
-                        Platform.runLater(() -> tabPane.getTabs().add(tab));
-                    } else if (buttonText.equals("open folder")) {
-                        Desktop desktop = Desktop.getDesktop();
-                        desktop.open(selectedFile);
-                    }
+                tabPane.getTabs().add(tab);
+            } else {
+                try {
+                    Desktop desktop = Desktop.getDesktop();
+                    desktop.open(selectedFile);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
-            }else if (result == JFileChooser.CANCEL_OPTION) {
-                System.out.println("cancelled");
             }
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
+        } else if (result == JFileChooser.CANCEL_OPTION) {
+            System.out.println("cancelled");
         }
     }
+
     public void SBClose() {
         if (clickCount % 2 == 0)
             splitPane.setDividerPosition(0, 1);
